228af3491267d1481d08f6351173e27deb38ba5f
Hevin <hevinvnsgu@gmail.com>
2024-12-06 12:19:02 +0530

    fix: update sitemap with static URLs and change frequencies

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,24 +1,42 @@
 import { MetadataRoute } from "next";
-import { headers } from "next/headers";
-import { allDocs, allPages } from "@/.contentlayer/generated";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const headersList = headers();
-  let domain = headersList.get("host") as string;
-  let protocol = "https";
-
   return [
     {
-      url: `${protocol}://${domain}`,
+      url: "https://leadsbundle.site/",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
     },
-    ...allPages.map((post) => ({
-      url: `${protocol}://${domain}/${post.slugAsParams}`,
+    {
+      url: "https://leadsbundle.site/about-us",
       lastModified: new Date(),
-    })),
-    ...allDocs.map((post) => ({
-      url: `${protocol}://${domain}/docs/${post.slugAsParams}`,
-      lastModified: post.date,
-    })),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://leadsbundle.site/contact-us",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://leadsbundle.site/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://leadsbundle.site/terms-and-conditions",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://leadsbundle.site/refund-policy",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
   ];
 }
